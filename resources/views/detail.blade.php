3c8051bdaf7deb076e2e9416f7b7b73443c84389
eliascristhian05 <elias012636@gmail.com>
2021-11-28 01:04:44 -0400

    img ejemplos

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -1,6 +1,6 @@
-<x-app-layout>
-    <x-slot name="header">
-</br>
+@extends('layouts.layout')
+
+@section('content')
 <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
 
 <div class="container">
@@ -19,7 +19,7 @@
                     {{-- Detalle productos --}}
                         <div class="row">
                                 <div class="col-6">
-                                    <img src="img/$product->imagen" width="300" height="300">
+                                    <img src="/img/{{$product->imagen}}" width="300" height="300">
                                     <h4>{{ $product->name }}</h4>
                                     <p>{{ $product->description }}</p>
                                     <p><strong>Precio: </strong> {{ $product->price }}Bs</p>
@@ -32,5 +32,4 @@
         </div>
     </div>
 </div>
-
-</x-app-layout>
+@endsection
